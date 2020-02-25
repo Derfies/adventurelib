42145d7269d1e691c5108724ea53579915b932ac
Derfies <Derfies@users.noreply.github.com>
2020-02-25 18:40:50 +1100

    [EDIT] - adventurelib.py - Reordered imports.

--- adventurelib.py
+++ adventurelib.py
@@ -1,8 +1,9 @@
+from __future__ import print_function
 import re
 import sys
 import inspect
 from builtins import input
-from __future__ import print_function
+
 try:
     import readline  # noqa: adds readline semantics to input()
 except ImportError:
